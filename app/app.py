c0150ec34821a99eccb772eeea7c10d303661937
FaboCloudsurf <FaboCloudsurf@gmail.com>
2026-09-05 23:31:25 -0400

    Flask Hello World app with health endpoint

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Dockerized Flask App!'
+
+@app.route('/health')
+def health():
+    return {'status': 'ok'}, 200
+
+if __name__ == '__main__':
+    # Host '0.0.0.0' allows the app to accept connections from outside its container
+    app.run(host='0.0.0.0', port=5001)
