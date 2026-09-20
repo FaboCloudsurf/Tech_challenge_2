dfc99778fec22cb16a3d11a1c5df8569edccd7ef
FaboCloudsurf <FaboCloudsurf@gmail.com>
2026-09-20 06:15:16 -0400

    test oidc

--- app/app.py
+++ app/app.py
@@ -11,5 +11,5 @@ def health():
     return {'status': 'ok'}, 200
 
 if __name__ == '__main__':
-    # Host '0.0.0.0' allows the app to accept connections from outside its containers
+    # Host '0.0.0.0' allows the app to accept connections from outside its container (test)
     app.run(host='0.0.0.0', port=5001)
